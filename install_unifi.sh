623d48b78bb63fa9dc8b9d2b1c664baa5ca9a157
nambrosch <nik@ambrosch.com>
2017-10-07 02:08:58 -0400

    reminders

--- install_unifi.sh
+++ install_unifi.sh
@@ -3,7 +3,8 @@
 # install ubiquiti unifi controller software
 # download url: https://dl.ubnt.com/unifi/5.4.11/UniFi.unix.zip
 
-# redhat requires oracle-java mongodb-server
+# reminder: disable selinux
+# reminder: install oracle-java, mongodb-server, unzip
 
 unifi_base=/opt/unifi
 
@@ -30,7 +31,7 @@ if [[ ! -w $unifi_base ]]; then
 fi
 
 # install mongodb-server
-dnf install mongodb-server -y
+dnf install unzip mongodb-server -y
 
 # stop if the requested version already exists on disk
 if [[ -d $unifi_base/$unifi_ver ]]; then
@@ -82,8 +83,8 @@ After=network.target
 Type=simple
 User=unifi
 WorkingDirectory=$unifi_base/$unifi_ver
-ExecStart=/usr/bin/java -Xmx512M -server -jar $unifi_base/$unifi_ver/lib/ace.jar start
-ExecStop=/usr/bin/java -jar $unifi_base/$unifi_ver/lib/ace.jar stop
+ExecStart=/usr/bin/java -Xmx512M -server -jar /opt/unifi/5.6.18-8261dc5066/lib/ace.jar start
+ExecStop=/usr/bin/java -jar /opt/unifi/5.6.18-8261dc5066/lib/ace.jar stop
 SuccessExitStatus=143
 
 [Install]
